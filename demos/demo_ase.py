bcac8fd898914cd230630bf87e0fafa2329d7bf4
Frederik Hvilshøj <fhvilshoj@gmail.com>
2025-03-04 23:03:54 +0100

    chore: rework the project to be more "modern"

--- demos/demo_ase.py
+++ demos/demo_ase.py
@@ -4,9 +4,11 @@ from ase.calculators.lj import LennardJones
 from ase.md.langevin import Langevin
 import ase
 
-atoms = ase.io.read('1crn.pdb')[:642] # remove water 
+atoms = ase.io.read("1crn.pdb")[:642]  # remove water
 atoms.calc = LennardJones()
-dyn = Langevin(atoms, 0.003/ase.units.fs, temperature_K=300, friction=0.002*ase.units.fs)
+dyn = Langevin(
+    atoms, 0.003 / ase.units.fs, temperature_K=300, friction=0.002 * ase.units.fs
+)
 logmd = LogMD()
 dyn.attach(lambda: logmd(atoms), interval=4)
 dyn.run(64)
